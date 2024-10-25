50eeaa5c63777da4daa86cc454720cb8eabb5998
pethoemilia <120325025+emilypetho@users.noreply.github.com>
2024-10-25 14:17:09 +0300

    profile page

--- Frontend/friends_with_memories/lib/pages/profile_page.dart
+++ Frontend/friends_with_memories/lib/pages/profile_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:friends_with_memories/pages/calendar_page.dart';
+import 'package:friends_with_memories/pages/edit_profile.dart';
 import 'package:friends_with_memories/pages/gallery_page.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final String profileImageUrl;
   final String userName;
   final List<Map<String, String>> friends;
@@ -15,15 +16,98 @@ class ProfilePage extends StatelessWidget {
   });
 
   @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  late String profileImageUrl;
+  late String userName;
+  late List<Map<String, String>> friends;
+
+  @override
+  void initState() {
+    super.initState();
+    profileImageUrl = widget.profileImageUrl;
+    userName = widget.userName;
+    friends = List.from(widget.friends); // Deep copy to prevent modifying the original list
+  }
+
+  void _addFriend() async {
+    final result = await showDialog<Map<String, String>>(
+      context: context,
+      builder: (BuildContext context) {
+        String newFriendName = '';
+        String newFriendImageUrl = '';
+
+        return AlertDialog(
+          title: const Text('Add Friend'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(hintText: 'Friend Name'),
+                onChanged: (value) {
+                  newFriendName = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(hintText: 'Friend Image URL'),
+                onChanged: (value) {
+                  newFriendImageUrl = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop({'name': newFriendName, 'imageUrl': newFriendImageUrl});
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result != null) {
+      setState(() {
+        friends.add(result);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // Eltávolítja a vissza nyilat
+        automaticallyImplyLeading: false,
         actions: [
           IconButton(
             icon: const Icon(Icons.edit),
-            onPressed: () {
-              // Edit profile logic
+            onPressed: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditProfilePage(
+                    initialProfileImageUrl: profileImageUrl,
+                    initialUserName: userName,
+                  ),
+                ),
+              );
+
+              if (result != null) {
+                setState(() {
+                  userName = result['userName'];
+                  profileImageUrl = result['profileImageUrl'];
+                });
+              }
             },
           ),
         ],
@@ -46,9 +130,7 @@ class ProfilePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () {
-                    // Add friend logic
-                  },
+                  onPressed: _addFriend,
                   child: const Text('Add Friend'),
                 ),
                 ElevatedButton(
